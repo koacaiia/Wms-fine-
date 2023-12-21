ab2041cdcd755d0e3de52796b1aa4644b082e381
koaca <koacaiia@gmail.com>
2023-12-21 16:32:27 +0900

    1221_fcmUpdated

---
+++ fbfunction.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase-admin";
+import{ getMessaging } from "@firebase/messaging-compat";
+
+const firebaseConfig ={
+    apiKey: "AIzaSyDLzmZyt5nZwCk98iZ6wi01y7Jxio1ppZQ",
+    authDomain: "fine-bondedwarehouse.firebaseapp.com",
+    databaseURL: "https://fine-bondedwarehouse-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fine-bondedwarehouse",
+    storageBucket: "fine-bondedwarehouse.appspot.com",
+    messagingSenderId: "415417723331",
+    appId: "1:415417723331:web:15212f190062886281b576",
+    measurementId: "G-SWBR4359JQ"
+}
+const app = initializeApp(firebaseConfig);
+const message = getMessaging(app);c
+console.log(message)
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts("https://www.gstatic.com/firebasejs/8.8.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.8.1/firebase-messaging.js");
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDLzmZyt5nZwCk98iZ6wi01y7Jxio1ppZQ",
+            authDomain: "fine-bondedwarehouse.firebaseapp.com",
+            databaseURL: "https://fine-bondedwarehouse-default-rtdb.asia-southeast1.firebasedatabase.app",
+            projectId: "fine-bondedwarehouse",
+            storageBucket: "fine-bondedwarehouse.appspot.com",
+            messagingSenderId: "415417723331",
+            appId: "1:415417723331:web:15212f190062886281b576",
+            measurementId: "G-SWBR4359JQ"
+};
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
